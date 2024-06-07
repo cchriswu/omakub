a07ceede253b44c8f7aaf8e0165e93a0e994bf9f
Chris Wu <caoweichriswu@gmail.com>
2024-06-06 22:06:21 -0700

    Remove things I don't need

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -3,6 +3,5 @@ sudo apt update
 sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
 	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
 fi
